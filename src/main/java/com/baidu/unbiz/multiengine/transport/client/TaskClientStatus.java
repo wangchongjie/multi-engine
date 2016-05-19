fa3e00538c34972095a36650c2c46007998e7472
wangchongjie <wangcj519@gmail.com>
2016-05-19 10:17:39 +0800

    comment

--- src/main/java/com/baidu/unbiz/multiengine/transport/client/TaskClientStatus.java
+++ src/main/java/com/baidu/unbiz/multiengine/transport/client/TaskClientStatus.java
@@ -3,6 +3,8 @@ package com.baidu.unbiz.multiengine.transport.client;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
+ * Task提交客户端实例状态
+ *
  * Created by wangchongjie on 16/5/5.
  */
 public class TaskClientStatus {
